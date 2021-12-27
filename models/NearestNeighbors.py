90f65105993508f27a5722f36fd5e529fc52f4fa
Ilias BALAH <ilias_balah@hotmail.com>
2021-12-27 02:22:23 +0100

    Update 2: adapte the code to wrok on coordinates data

--- models/NearestNeighbors.py
+++ models/NearestNeighbors.py
@@ -8,7 +8,9 @@ class NearestNeighbors:
     def __init__(self) -> None:
         self.__data: Graph = None
 
-    def _get_optimal_path(self, vertex: Vertex, visited_vertices=[]):
+    def _get_optimal_path(self, vertex=None, visited_vertices=[]):
+        if not vertex:
+            vertex = self.__data.get_vertices()[0]
         nearest_neighbors, nearest_neighbor_distance = vertex.get_nearest_neighbor(
             exclude=visited_vertices)
         if not nearest_neighbors:
@@ -24,17 +26,16 @@ class NearestNeighbors:
         return optimal_path, optimal_path_length
 
     def _optimize(self):
-        optimal_path = []
-        optimal_path_length = float("inf")
-        for vertex in self.__data.get_vertices():
-            path, path_length = self._get_optimal_path(vertex)
-            print(f"Nearest Neighbor Cycle ({vertex} as depot):", path_length)
-            if path_length < optimal_path_length:
-                optimal_path = [path]
-                optimal_path_length = path_length
-            elif path_length == optimal_path_length:
-                optimal_path.append(path)
-        return (optimal_path, optimal_path_length)
+        # optimal_path = []
+        # optimal_path_length = float("inf")
+        # for vertex in self.__data.get_vertices():
+        #     path, path_length = self._get_optimal_path(vertex)
+        #     if path_length < optimal_path_length:
+        #         optimal_path = [path]
+        #         optimal_path_length = path_length
+        #     elif path_length == optimal_path_length:
+        #         optimal_path.append(path)
+        return self._get_optimal_path()
 
     def fit(self, data):
         self.__data = data
